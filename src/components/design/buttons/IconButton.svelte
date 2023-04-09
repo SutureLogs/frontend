df2fd7e2ef0672e7af63b7d03f11c222870d369d
yajatvishwakk <yajat472001@gmail.com>
2023-04-09 09:50:24 +0530

    browse page done

---
+++ src/components/design/buttons/IconButton.svelte
@@ -0,0 +1,7 @@
+<script>
+  export let icon;
+</script>
+
+<button class="btn rounded-full">
+  <img src={icon} class="w-5 h-5" alt="" />
+</button>
--- src/components/design/cards/BrowseCard.svelte
+++ src/components/design/cards/BrowseCard.svelte
@@ -1,72 +1,71 @@
+<script>
+  export let surgeryName;
+  export let doctorName;
+  export let doctorTitle;
+  export let doctorImage;
+  export let surgeryImage;
+  export let orgName;
+  //https://d2csxpduxe849s.cloudfront.net/media/E32629C6-9347-4F84-81FEAEF7BFA342B3/1968EA5E-28A1-49D1-A27CC556F7B8DF3C/C3E67292-300A-4B3F-AF06F349CDFBB918/WebsiteJpg_XL-FSURG_Main%20Visual_Cyan_Website.jpg
+  // https://hips.hearstapps.com/hmg-prod/images/portrait-of-a-happy-young-doctor-in-his-clinic-royalty-free-image-1661432441.jpg?crop=0.66698xw:1xh;center,top&resize=1200:*
+</script>
+
 <div class="flex flex-col lg:hidden">
   <img
-    src="https://d2csxpduxe849s.cloudfront.net/media/E32629C6-9347-4F84-81FEAEF7BFA342B3/1968EA5E-28A1-49D1-A27CC556F7B8DF3C/C3E67292-300A-4B3F-AF06F349CDFBB918/WebsiteJpg_XL-FSURG_Main%20Visual_Cyan_Website.jpg"
+    src={surgeryImage}
     alt=""
     class="aspect-video w-full object-cover rounded-xl"
   />
-  <div class="text-xl font-bold mt-3 line-clamp-2">
-    Hand‑assisted laparoscopic surgery for an esophageal hiatal hernia with
-    incarcerated transverse colon presenting after laparoscopic gastrectomy
+  <div class="text-xl font-semibold mt-3 line-clamp-2">
+    {surgeryName}
   </div>
   <div class="flex items-center gap-3 mt-5">
     <div>
       <img
-        src="https://hips.hearstapps.com/hmg-prod/images/portrait-of-a-happy-young-doctor-in-his-clinic-royalty-free-image-1661432441.jpg?crop=0.66698xw:1xh;center,top&resize=1200:*"
+        src={doctorImage}
         alt=""
         class="w-10 h-10 rounded-full border-2 border-blue-400"
       />
     </div>
     <div class="flex flex-col">
-      <div class=" font-bold">Danny boi</div>
-      <div class="text-sm">Cardiovascular surgeon</div>
+      <div class=" font-bold">{doctorName}</div>
+      <div class="text-sm">{doctorTitle}</div>
     </div>
   </div>
 </div>
 
-<div class="hidden lg:flex flex-col">
+<div
+  class="hidden lg:flex flex-col hover:scale-[98%] cursor-pointer transition-all duration-200"
+>
   <div class="flex gap-4 items-center">
     <div>
       <img
-        src="https://hips.hearstapps.com/hmg-prod/images/portrait-of-a-happy-young-doctor-in-his-clinic-royalty-free-image-1661432441.jpg?crop=0.66698xw:1xh;center,top&resize=1200:*"
+        src={doctorImage}
         alt=""
         class="w-12 h-12 rounded-full border-2 border-blue-400"
       />
     </div>
     <div>
       <div class="flex gap-3 items-center">
-        <div class="text- font-bold">Danny boi</div>
+        <div class="text- font-bold">{doctorName}</div>
         <div class="w-2 h-2 bg-primary rounded-full" />
-        <div class="">Cardiovascular surgeon</div>
+        <div class="">{doctorTitle}</div>
       </div>
       <!-- svelte-ignore a11y-label-has-associated-control -->
       <label
         class="label-text-alt capitalize py-2 font-bold text-[#4669C1] tracking-wider"
       >
-        Saptagiri Medical School</label
+        {orgName}</label
       >
-    </div>
-    <div class="ml-auto text-red-500">
-      <svg
-        xmlns="http://www.w3.org/2000/svg"
-        viewBox="0 0 24 24"
-        fill="currentColor"
-        class="w-6 h-6"
-      >
-        <path
-          d="M11.645 20.91l-.007-.003-.022-.012a15.247 15.247 0 01-.383-.218 25.18 25.18 0 01-4.244-3.17C4.688 15.36 2.25 12.174 2.25 8.25 2.25 5.322 4.714 3 7.688 3A5.5 5.5 0 0112 5.052 5.5 5.5 0 0116.313 3c2.973 0 5.437 2.322 5.437 5.25 0 3.925-2.438 7.111-4.739 9.256a25.175 25.175 0 01-4.244 3.17 15.247 15.247 0 01-.383.219l-.022.012-.007.004-.003.001a.752.752 0 01-.704 0l-.003-.001z"
-        />
-      </svg>
     </div>
   </div>
   <div class="mt-4">
     <img
-      src="https://d2csxpduxe849s.cloudfront.net/media/E32629C6-9347-4F84-81FEAEF7BFA342B3/1968EA5E-28A1-49D1-A27CC556F7B8DF3C/C3E67292-300A-4B3F-AF06F349CDFBB918/WebsiteJpg_XL-FSURG_Main%20Visual_Cyan_Website.jpg"
+      src={surgeryImage}
       alt=""
       class="aspect-video w-full object-cover rounded-xl"
     />
-    <div class="p-2 text-lg font-bold">
-      Hand‑assisted laparoscopic surgery for an esophageal hiatal hernia with
-      incarcerated transverse colon presenting after laparoscopic gastrectomy
+    <div class="p-2 text-lg font-semibold">
+      {surgeryName}
     </div>
   </div>
 </div>
