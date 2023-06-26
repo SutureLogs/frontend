10ecdc4cf24f2be3d51cf4c949f68f066354e208
yajatvishwakk <yajat472001@gmail.com>
2023-06-26 23:01:04 +0530

    final commit

--- src/components/design/buttons/PatientSurgeryButton.svelte
+++ src/components/design/buttons/PatientSurgeryButton.svelte
@@ -17,8 +17,8 @@
   }}
   class="border-2 cursor-pointer w-full p-5 border-r-0 hover:opacity-100 pl-8 border-b-0 border-t-0 transition flex flex-col {isActive
     ? 'border-l-[10px] border-[#6990d8] bg-gradient-to-r from-[#82aaf4] via-transparent to-slate-50 bg-opacity-0'
-    : 'hover:border-l-[10px] hover:border-gray-300 opacity-25'}"
+    : 'hover:border-l-[10px] hover:border-gray-300 opacity-70'}"
 >
-  <Label styleClass="opacity-50">Surgery {index}</Label>
+  <Label styleClass="opacity-70">Surgery {index}</Label>
   <h1 class="text-lg font-semibold">{surgeryName}</h1>
 </button>
